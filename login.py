0ad8db42619b3bbabe68d343389e08754a309234
FibZIII <soloninandr@gmail.com>
2022-01-27 15:11:32 +0300

    Add login gor get session

---
+++ login.py
@@ -0,0 +1,8 @@
+# file: login.py
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+from config import (API_ID, API_HASH)
+
+# Авторизация и печать ключа сессии
+with TelegramClient(StringSession(), API_ID, API_HASH) as client:
+    print(client.session.save())
